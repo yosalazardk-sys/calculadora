dc2165385306be976ee8494f23811ae4db5ed7af
kristianSalazar <yosalazardk@gmail.com>
2026-03-01 21:26:42 -0500

    calculadora_script_completo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert("moñoñongo");
+let pantalla = document.getElementById("pantalla");
+
+function agregar(valor){
+    pantalla.value += valor;
+}
+
+function limpiar(){
+    pantalla.value = "";
+}
+
+function borrar(){
+    pantalla.value = pantalla.value.slice(0, -1)
+}
+
+function calcular(){
+    try {
+        let expresion = pantalla.value;
+
+        expresion = expresion.replace(/(\d+)%/g,"$1/100");
+        pantalla.value = eval(expresion);
+        
+    } catch (error) {
+        pantalla.value = "Error";
+    }
+}
